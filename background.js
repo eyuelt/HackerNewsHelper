3b0c7052b135d78ceb00445be9568e3cc3a3bb8e
eyuelt <eyueltessema@gmail.com>
2013-04-09 04:52:06 -0700

    Clean up code

--- background.js
+++ background.js
@@ -71,7 +71,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 			chrome.tabs.update(tab.id, {url: targetURL});
 		} else { //we are already on the HN homepage
 			//toggle between options 1, 2, 3
-			var badgeColor = [255, 0, 0];
 			getState(function(state) {
 					setState((state % 3) + 1, function() {
 							updateIcon(tab.id);
@@ -79,34 +78,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 				});
 		}
 	});
-
-//color is an RGB color array, alpha is the A value in the returned RGBA color array
-function colorWithAlpha(color, alpha) {
-	return color.concat(alpha);
-}
-
-//fades badge out by reducing opacity over time and then removing the badge altogether
-function fadeOutBadge() {
-	chrome.browserAction.getBadgeBackgroundColor({}, function(result) {
-			var color = result.slice(0,3);
-			var alpha = result[3];
-			
-			var reduceDelta = 50;
-			var timeInterval = 50;
-			var x = setInterval(function() {
-					if (alpha - reduceDelta <= 0) {
-						clearInterval(x);
-						chrome.browserAction.setBadgeText({text: ""});
-						chrome.browserAction.setBadgeBackgroundColor({color: colorWithAlpha(color, 255)});
-					} else {
-						chrome.browserAction.getBadgeBackgroundColor({}, function(result) {
-								alpha -= reduceDelta;
-								chrome.browserAction.setBadgeBackgroundColor({color: colorWithAlpha(color, alpha)});
-							});
-					}
-				}, timeInterval);
-		});
-}
 
 //set the icon image to the icon represented by the current state
 function updateIcon(tabId) {
@@ -145,6 +116,5 @@ function updateAllHNTabIcons() {
 					updateIcon(HNTabs[i].id);
 				}
 			});
-		alert("starting extension");
 	} catch(e) { alert(e); }
 }
